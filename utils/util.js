a1504e699daee5ca9c2e320ad5d05ad6548a58f7
yubinchen <928492516+yubinchen@users.noreply.github.com>
2019-10-05 22:37:29 +0800

    complete the httpRequest interface

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,9 @@
+module.exports = {
+  formatTime: formatTime,
+  httpRequest: httpRequest
+}
+const baseUrl = "http://127.0.0.1:8000/";//测试环境
+//const baseUrl = "https://www.luckydraw.net.cn/"; //正式环境
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -10,10 +16,59 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-
-module.exports = {
-  formatTime: formatTime
+/**
+ * loading:是否显示loading，参数为true/false
+ * url:变化的url后缀
+ * sessionChoose:headerType,0为json，1为x-www-form-urlencoded
+ * data:传输的数据
+ * method:'POST'/'GET'，0为POST,1为GET
+ * callBack:回调函数,传入的参数格式为function(res)
+ */
+function httpRequest(loading, url, sessionChoose, params, method, callBack_success){
+  var headerType = [{ 'content-type': 'application/json' }, { 'content-type': 'application/x-www-form-urlencoded' }]
+  var methodType = ['POST', 'GET']
+  if (loading == true) {
+    wx.showToast({
+      title: '数据加载中',
+      icon: 'loading'
+    })
+  }
+  wx.request({
+    url: baseUrl + url,
+    data: params,
+    dataType: "json",
+    header: headerType[sessionChoose],
+    method: methodType[method],
+    success: function (res) {
+      if (loading == true) {
+        wx.hideToast(); //隐藏提示框
+      }
+      console.log('调用/utils/util.js/httpRequest函数成功')
+      callBack_success(res.data);
+    },
+    fail: function (res) {
+      console.log(res);
+      wx.showModal({
+        title: '提示',
+        content: '请求失败！由于网络请求时间过长或网络无法连接的原因，请确认网络畅通，点击"重新请求"进行再次请求！',
+        confirmText: "重新请求",
+        success: function (res) {
+          if (res.confirm) {
+            httpRequst(loading, url, sessionChoose, params, method, callBack_success, callBack_failed);//再次进行请求
+          } else if (res.cancel) {
+            console.log('用户点击取消');
+          }
+        }
+      })
+    },
+    complete: function () {
+      if (loading == true) {
+        wx.hideToast(); //隐藏提示框
+      }
+    }
+  })
 }
+
 
 class PrizeInformation {
   constructor(String1, String2, number1, number2, String3, String4, Data) {
